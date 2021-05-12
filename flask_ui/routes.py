8c1859c8ea07d1f9be20b1307609e62754c7ca43
Joshua Rowley <joshuarowley42@gmail.com>
2021-05-12 23:32:27 +0100

    more useful view

--- flask_ui/routes.py
+++ flask_ui/routes.py
@@ -2,7 +2,7 @@ from flask_ui import app
 
 from config import *
 from octopus import OctopusClient
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 energy_provider = OctopusClient(username=OCTOPUS_USERNAME,
                                 zone=OCTOPUS_ZONE,
@@ -13,15 +13,7 @@ energy_provider = OctopusClient(username=OCTOPUS_USERNAME,
 
 
 @app.route('/')
-@app.route('/index')
 def index():
-    prices = energy_provider.get_elec_price(start_time=datetime(year=2021,
-                                                                month=5,
-                                                                day=9,
-                                                                tzinfo=timezone.utc),
-                                            end_time=datetime(year=2021,
-                                                              month=5,
-                                                              day=11,
-                                                              hour=1,
-                                                              tzinfo=timezone.utc))
+    start = datetime.now(tz=timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=20)
+    prices = energy_provider.get_elec_price(start_time=start)
     return {str(p): prices[p] for p in prices}
