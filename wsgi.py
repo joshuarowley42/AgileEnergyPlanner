9b93f3e6428b261aab8f3321556da4c329c6cc6f
Joshua Rowley <joshuarowley42@gmail.com>
2021-05-13 08:30:51 +0100

    import app already instantiated

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,5 @@
 """Application entry point."""
-from flask_ui import init_app
-
-
-app = init_app()
-
+from flask_ui import app
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
